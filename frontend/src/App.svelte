170a2981ed78f1d8a81ba0347bc2fda3a57c56fb
bagiicpp <bagii@bagii-arch.localdomain>
2026-02-07 18:00:19 +0100

    repo setup

---
+++ frontend/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+  import { onMount } from "svelte";
+  let videoElement: HTMLVideoElement;
+
+
+  onMount(async () => {
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({video: true})
+    
+      videoElement.srcObject = stream
+      videoElement.play()
+    } catch (err) {
+      console.error(err)
+    }
+    
+  })
+
+</script>
+
+<main>
+  <h1 class="font-bold text-6xl">Hiii</h1>
+
+  <video bind:this={videoElement} autoplay playsinline height="800px" width="800px"></video>
+
+</main>
